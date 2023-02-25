63a8bf71cbafe9c9c8107d1177e740cd7a39efd1
Ramya Bygari <ramyabygari@wirelessprv-10-193-140-134.near.illinois.edu>
2023-02-25 13:30:15 -0600

    edited scripts for  multiple clients

--- scripts/multiple_clients.sh
+++ scripts/multiple_clients.sh
@@ -4,50 +4,25 @@
 sudo apt-get update
 sudo apt-get install parallel
 
+clients=2
+
 # change working directory from scripts
 pushd ../
-echo $(pwd)
-# rm -rf inputs
-# mkdir inputs
-# rm -rf outputs
-# mkdir outputs
-
 
 echo $(pwd)
 
+# Compile
 rm -rf src/abd-algo/cmake/build
 mkdir -p src/abd-algo/cmake/build
 pushd src/abd-algo/cmake/build
 cmake -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl -DOPENSSL_LIBRARIES=/opt/homebrew/opt/openssl/lib -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
 make -j 4
-
 popd
 
 # make one client initialise 
-# echo $(pwd)
-
-# echo $(pwd)
-# python3 ./benchmark/random_gen.py W
-
 pushd src/abd-algo/cmake/build
 ./abd_client true input.txt input.txt
 popd
-
-# pushd inputs/
-
-# echo $(pwd)
-
-# # generate x clients workload
-# # generate read_workload
-# clients=10
-# for i in $(eval echo {1..$clients})
-# do 
-#     python3 ./../benchmark/read_workload.py $i
-#     echo "Client $i read workload generated"
-# done
-
-# popd
-# echo $(pwd)
 
 
 export SHELL=$(type -p bash)
@@ -59,14 +34,13 @@ run_cpp_executable() {
     ./abd_client false  ${input_file} ${output_file}
 }
 
-
 export -f run_cpp_executable
-
 
 echo $(pwd)
 # List of read_workload nput files to process
 pushd inputs/read_workload
 input_files=$(ls ./read_workload_input*.txt)
+input_files=("${input_files[@]:0:clients}")
 popd
 
 #  Run gnu-parallel
