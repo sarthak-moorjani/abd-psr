577571282f6a6b194dfbecf6fa47ec393ba3cf9d
Ramya Bygari <ramyabygari@Ramyas-MacBook-Pro.local>
2023-02-26 16:51:30 -0600

    update version of etcd

--- etcd/scripts/etcd.sh
+++ etcd/scripts/etcd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /usr/local/src
-sudo wget "https://github.com/coreos/etcd/releases/download/v3.3.9/etcd-v3.3.9-linux-amd64.tar.gz"
-sudo tar -xvf etcd-v3.3.9-linux-amd64.tar.gz
-sudo mv etcd-v3.3.9-linux-amd64/etcd* /usr/local/bin/
+sudo wget "https://github.com/coreos/etcd/releases/download/v3.4.1/etcd-v3.4.1-linux-amd64.tar.gz"
+sudo tar -xvf etcd-v3.4.1-linux-amd64.tar.gz
+sudo mv etcd-v3.4.1-linux-amd64/etcd* /usr/local/bin/
 sudo mkdir -p /etc/etcd /var/lib/etcd
 sudo groupadd -f -g 1501 etcd
 sudo useradd -c "etcd user" -d /var/lib/etcd -s /bin/false -g etcd -u 1501 etcd
