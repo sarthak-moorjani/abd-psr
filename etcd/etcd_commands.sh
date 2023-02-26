736282b721733e340c2755c1a49125c748ca2b83
Ramya Bygari <ramyabygari@Ramyas-MacBook-Pro.local>
2023-02-26 14:26:35 -0600

    etcd uncomment input

--- etcd/etcd_commands.sh
+++ etcd/etcd_commands.sh
@@ -1,30 +1,30 @@
 # #!/bin/bash
 
-# commands_file="input.txt"
+commands_file="input.txt"
 
 
-# start_time=$(date +%s.%N)
+start_time=$(date +%s.%N)
 
-# while read command; do
+while read command; do
 
-#   eval $command
-# done < $commands_file
-
-
-# end_time=$(date +%s.%N)
-# time_taken=$(echo "$end_time - $start_time" | bc)
+  eval $command
+done < $commands_file
 
 
-# echo "Time taken: $time_taken seconds"
-
-start=$(date +%s.%N)
-
-cat input_kv.txt | xargs -L1 sh -c 'ETCDCTL_API=3 etcdctl put "$0" "$1"' {}
-
-end=$(date +%s.%N)
+end_time=$(date +%s.%N)
+time_taken=$(echo "$end_time - $start_time" | bc)
 
 
-runtime=$(echo "$end - $start" | bc)
+echo "Time taken: $time_taken seconds"
+
+# start=$(date +%s.%N)
+
+# cat input_kv.txt | xargs -L1 sh -c 'ETCDCTL_API=3 etcdctl put "$0" "$1"' {}
+
+# end=$(date +%s.%N)
+
+
+# runtime=$(echo "$end - $start" | bc)
 
 echo "Time taken: $runtime seconds" >> time.txt
 
