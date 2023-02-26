e622254252fdb1dd4f4fc8a65711e11d314f1163
Ramya Bygari <ramyabygari@wirelessprv-10-193-140-134.near.illinois.edu>
2023-02-25 22:52:46 -0600

    script to start etcd on all machines

---
+++ etcd/etcd_start_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Error: Expects Cloudlab Username (Case-sensitive)"
+  exit 1
+fi
+
+USER=$1
+declare -a nodes=("$USER@clnode288.clemson.cloudlab.us" "$USER@clnode315.clemson.cloudlab.us" "$USER@clnode294.clemson.cloudlab.us" "$USER@clnode304.clemson.cloudlab.us" "$USER@clnode303.clemson.cloudlab.us")
+
+for i in "${nodes[@]}"
+do
+    ssh -i ~/.ssh/id_ed25519 -t $i 'sudo systemctl daemon-reload && sudo systemctl enable etcd && sudo systemctl start etcd.service && sudo systemctl status -l etcd.service'
+done
